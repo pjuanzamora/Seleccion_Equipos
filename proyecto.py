5e6e0535dc3cb79b0de21fa1442c00a682b4fd66
Juan Francisco <pjuanzamora@gmail.com>
2023-01-19 10:18:34 +0100

    Imagenes terminado

--- proyecto.py
+++ proyecto.py
@@ -4,22 +4,36 @@ import flet as ft
 
 def main(page: ft.Page):
    
+    def cambiar_imagen(e):
+        if (menu.value == "Madrid"):
+            img.src = "Madrid.png"
+            print("hola")
+        elif (menu.value == "Barsa"):
+            img.src = "Barsa.png"
+        else:
+            img.src = "SinImagen.png"
+        page.update()
+
+
+
+
     vEquipos = ["Madrid", "Barsa", "Mogón","Villacarrillo"]
     vEquiposSelecionados = []
     
-    menu = ft.Dropdown(width=100)
+    menu = ft.Dropdown(hint_text="Selecciona un equipo",width=250, on_change=cambiar_imagen)
 
     for equipo in vEquipos:
         menu.options.append(ft.dropdown.Option(equipo))
 
     
-
-    #ft.dropdown.Option("Red"),
-
-    page.add(menu)
+    img = ft.Image(src=f"a")
+    
 
 
+    page.add(menu,img)
 
 
 
-ft.app(target=main)
+
+
+ft.app(target=main,assets_dir="imagenes")
